4606868931ca942e440bf2417fc3d3d29c7796d0
Eunmin Kim <telepopsound@gmail.com>
2014-09-22 21:31:02 +0900

    init

---
+++ cider-set-ns-and-eval-buffer.el
@@ -0,0 +1,17 @@
+(defun get-active-minor-mode-list ()
+  (let ((active-modes))
+    (mapc (lambda (mode) (condition-case nil
+                         (if (and (symbolp mode) (symbol-value mode))
+                             (add-to-list 'active-modes mode))
+                       (error nil)))
+          minor-mode-list)
+    active-modes))
+
+(defun cider-set-ns-and-eval-buffer ()
+  (when (member 'cider-mode (get-active-minor-mode-list))
+    (cider-repl-set-ns (cider-current-ns))
+    (if (not (equal "user" (cider-current-ns)))
+        (cider-load-current-buffer))))
+
+(add-hook 'after-save-hook 'cider-set-ns-and-eval-buffer)
+(add-hook 'window-configuration-change-hook 'cider-set-ns-and-eval-buffer) 
